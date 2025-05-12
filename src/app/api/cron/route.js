d16133fac8cd203e536c54ad994cada604d4a998
ikaz-jem <echchebabzakariae@gmail.com>
2025-05-12 11:43:36 +0100

    cron added 1 min

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -3,11 +3,9 @@ import UserSchema from '@/app/models/userSchema/UserSchema';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
-
 const connection = await dbConnect()
-
 const user = await UserSchema.findOne({email:"echchebabzakariae@gmail.com" })
 user.balance +=1
 await user.save()
-  return NextResponse.json({ ok: true });
+  return NextResponse.json({ ok: true ,message:'cron updated'});
 }
