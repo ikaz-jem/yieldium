3a85baffea0c1e5d68b46898887160efa1ad47ee
ikaz-jem <echchebabzakariae@gmail.com>
2025-05-12 11:41:22 +0100

    added cron job

--- src/app/api/cron/route.js
+++ src/app/api/cron/route.js
@@ -1,5 +1,13 @@
+import dbConnect from '@/app/lib/db';
+import UserSchema from '@/app/models/userSchema/UserSchema';
 import { NextResponse } from 'next/server';
 
 export async function GET() {
+
+const connection = await dbConnect()
+
+const user = await UserSchema.findOne({email:"echchebabzakariae@gmail.com" })
+user.balance +=1
+await user.save()
   return NextResponse.json({ ok: true });
 }
