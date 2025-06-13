8be4ee75451966d5af4ca80f30d2b1ca9e2edf47
ikaz-jem <echchebabzakariae@gmail.com>
2025-06-13 12:24:47 +0100

    finished Contracts

--- src/app/dashboard/contracts/StakingPreview.jsx
+++ src/app/dashboard/contracts/StakingPreview.jsx
@@ -24,7 +24,7 @@ export default function StakingPreview({ contracts }) {
         if (filter === 'forced') return contract.forced
     })
 
-    
+
     function ComponentFilter() {
 
         if (filter == "active") return <Active setFilter={setFilter} contracts={filteredContracts} />
@@ -47,7 +47,7 @@ export default function StakingPreview({ contracts }) {
 
 
 function Active({ contracts }) {
-
+const now = new Date();
 
     if (contracts.length == 0) return (
         <div className='flex flex-col  w-full gap-2 border border-primary/10 p-5 bg-card rounded backdrop-blur-xl relative overflow-hidden '>
@@ -69,52 +69,58 @@ function Active({ contracts }) {
                 <h1 className="!text-neutral text-sm" >Investments</h1>
             </div>
             <div className='flex gap-5 flex-wrap   w-full '>
-                {contracts?.map((contract, idx) =>
-                    <div key={idx} className='flex flex-col max-w-xl w-full gap-2 border border-primary/10 p-5 bg-card rounded backdrop-blur-xl relative overflow-hidden '>
-                        <BorderEffect />
+                {contracts?.map((contract, idx) => {
+                    
+                    const target = new Date(contract.unlocksAt);
+                    const diffMs = target.getTime() - now.getTime();
+                    let disabled = diffMs >= 0
+                    return (
+                        <div key={idx} className='flex flex-col max-w-xl w-full gap-2 border border-primary/10 p-5 bg-card rounded backdrop-blur-xl relative overflow-hidden '>
+                            <BorderEffect />
 
-                        <div className='flex  gap-5 items-center '>
+                            <div className='flex  gap-5 items-center '>
 
-                            <img src='/assets/images/crypto/usdt.svg' alt="" className='w-8 h-8' />
-                            <div className='w-full flex justify-between'>
-                                <div className='flex flex-col '>
-                                    <h1>Tether</h1>
-                                    <p className='text-xs'>{parseFloat((contract?.amount).toFixed(2))} USDT</p>
-                                    {/* <p className='text-xs'>{balance?.prices[balance?.currency]}$</p> */}
+                                <img src='/assets/images/crypto/usdt.svg' alt="" className='w-8 h-8' />
+                                <div className='w-full flex justify-between'>
+                                    <div className='flex flex-col '>
+                                        <h1>Tether</h1>
+                                        <p className='text-xs'>{parseFloat((contract?.amount).toFixed(2))} USDT</p>
+                                        {/* <p className='text-xs'>{balance?.prices[balance?.currency]}$</p> */}
+                                    </div>
+                                    <p className='text-sm !text-green-500'>{parseFloat((contract?.profits).toFixed(2)) + ' $'} </p>
                                 </div>
-                                <p className='text-sm !text-green-500'>{parseFloat((contract?.profits).toFixed(2)) + ' $'} </p>
                             </div>
-                        </div>
 
-                        <div className="flex justify-between ">
-                            <p className='text-sm'>{timeLeft(contract.unlocksAt)} </p>
-                            <p className='text-md !text-red-500 font-bold'>{contract.claimed && "Claimed"} </p>
-                        </div>
-
-
-                        <div className="flex justify-between ">
-
-                            <p className='text-sm'>{formatISO(contract.unlocksAt)} </p>
-                            <div className="flex items-center gap-2">
-                                <p className='text-sm'>{contract.duration} days </p>
-
-                                {
-                                    contract?.isLocked ?
-                                        <FaLock className="text-yellow-500" />
-                                        :
-                                        <FaUnlock className="text-yellow-500" />
-                                }
-
+                            <div className="flex justify-between ">
+                                <p className='text-sm'>{timeLeft(contract.unlocksAt)} </p>
+                                <p className='text-md !text-red-500 font-bold'>{contract.claimed && "Claimed"} </p>
                             </div>
-                        </div>
 
 
-                        <div className="flex gap-2">
-                            <ButtonPrimary disabled >Claim</ButtonPrimary>
-                            <ForceUnlockModal contract={contract} />
-                        </div>
+                            <div className="flex justify-between ">
 
-                    </div>
+                                <p className='text-sm'>{formatISO(contract.unlocksAt)} </p>
+                                <div className="flex items-center gap-2">
+                                    <p className='text-sm'>{contract.duration} days </p>
+
+                                    {
+                                        disabled?
+                                            <FaLock className="text-yellow-500" />
+                                            :
+                                            <FaUnlock className="text-yellow-500" />
+                                    }
+
+                                </div>
+                            </div>
+
+
+                            <div className="flex gap-2">
+                                <ButtonPrimary disabled={disabled} >Claim</ButtonPrimary>
+                               {disabled && <ForceUnlockModal contract={contract} />}
+                            </div>
+
+                        </div>)
+                }
                 )
                 }
 
@@ -186,7 +192,7 @@ function Claimed({ contracts }) {
 
                         <div className="flex justify-between ">
                             <p className='text-sm'>Total Claimed : </p>
-                                <p className=' !text-green-500'>{parseFloat((contract?.amountClaimed)?.toFixed(2)) + ' $'} </p>
+                            <p className=' !text-green-500'>{parseFloat((contract?.amountClaimed)?.toFixed(2)) + ' $'} </p>
                         </div>
 
 
@@ -267,7 +273,7 @@ function Forced({ contracts }) {
 
                         <div className="flex justify-between ">
                             <p className='text-sm'>Total Claimed : </p>
-                                <p className=' !text-green-500'>{parseFloat((contract?.amountClaimed)?.toFixed(2)) + ' $'} </p>
+                            <p className=' !text-green-500'>{parseFloat((contract?.amountClaimed)?.toFixed(2)) + ' $'} </p>
                         </div>
 
 
